e467a4b1cab278a00dcecdb8c91809caf0a94810
js6pak <kubastaron@hotmail.com>
2018-12-24 23:25:52 +0100

    Add project files.

---
+++ kuvuBot/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.EventArgs;
+using Newtonsoft.Json;
+
+namespace kuvuBot
+{
+    class Config
+    {
+        [JsonProperty("token")] public string Token { get; set; }
+    }
+
+    class Program
+    {
+        static DiscordClient Client { get; set; }
+        static async void Main(string[] args)
+        {
+            var config = JsonConvert.DeserializeObject<Config>(File.ReadAllText("config.json"));
+            var cfg = new DiscordConfiguration
+            {
+                Token = config.Token,
+                TokenType = TokenType.Bot,
+
+                AutoReconnect = true,
+                LogLevel = LogLevel.Debug,
+                UseInternalLogHandler = true
+            };
+
+            Client = new DiscordClient(cfg);
+
+            Client.Ready += Client_Ready;
+
+            await Client.ConnectAsync();
+
+            // prevent app from quit
+            await Task.Delay(-1);
+        }
+
+        private static Task Client_Ready(ReadyEventArgs e)
+        {
+            Console.WriteLine("Discord bot is ready!");
+            return Task.CompletedTask;
+        }
+    }
+}
