c25e7680fe8ab1adf026916abd659c4191fcc862
js6pak <kubastaron@hotmail.com>
2018-12-26 20:17:27 +0100

    Add figlet command

---
+++ kuvuBot/Commands/Fun/FigletCommand.cs
@@ -0,0 +1,31 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using Colorful;
+using DSharpPlus.Entities;
+using System.Reflection;
+using System.IO;
+
+namespace kuvuBot.Commands.Fun
+{
+    public class FigletCommand : BaseCommandModule
+    {
+        [Command("figlet"), Description("Generates a figlet")]
+        public async Task Figlet(CommandContext ctx, [RemainingText] string message)
+        {
+            // load figlet font from assembly (.exe)
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "kuvuBot.big.flf";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                var figlet = new Figlet(FigletFont.Load(stream));
+                await ctx.RespondAsync($"```{figlet.ToAscii(message).ConcreteValue}```");
+            }
+        }
+    }
+}
