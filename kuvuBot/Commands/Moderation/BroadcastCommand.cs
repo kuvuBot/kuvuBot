03aa1ba222c8bf22055db86164bc8a11c990b114
kuvuś <kuba17ism@tuta.io>
2018-12-26 23:28:17 +0100

    Add broadcast command

---
+++ kuvuBot/Commands/Moderation/BroadcastCommand.cs
@@ -0,0 +1,39 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using HSNXT.DSharpPlus.ModernEmbedBuilder;
+using kuvuBot.Lang;
+using DSharpPlus;
+
+namespace kuvuBot.Commands.Moderation
+{
+    public class BroadcastCommand : BaseCommandModule
+    {
+        [Aliases("ogłoszenie", "ogloszenie", "bc")]
+        [Command("broadcast"), Description("Makes broadcast")]
+        public async Task Broadcast(CommandContext ctx, [RemainingText, Description("Broadcast")] string content = null)
+        {
+            if (content == null)
+            {
+                await ctx.RespondAsync(await ctx.Lang("global.badArguments"));
+                return;
+            }
+            if (!await ctx.HasPermission(Permissions.ManageGuild))
+            {
+                return;
+            }
+            var message = await new ModernEmbedBuilder
+            {
+                Title = await ctx.Lang("broadcast.broadcast"),
+                Description = content,
+                Color = new DuckColor(33, 150, 243),
+                Timestamp = DuckTimestamp.Now,
+                Footer = ($"Generated for {ctx.User.Username}#{ctx.User.Discriminator}", ctx.User.AvatarUrl),
+            }.Send(ctx.Message.Channel);
+        }
+    }
+}
