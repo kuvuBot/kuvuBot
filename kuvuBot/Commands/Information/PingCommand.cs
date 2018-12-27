22591aa95904dcfa7edf201f72d1539874d3a249
kuvuś <kuba17ism@tuta.io>
2018-12-27 21:44:23 +0100

    Add ping command

---
+++ kuvuBot/Commands/Information/PingCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace kuvuBot.Commands.Information
+{
+    public class PingCommand : BaseCommandModule
+    {
+        [Command("ping"), Description("Shows bot ping")]
+        public async Task Dice(CommandContext ctx)
+        {
+            await ctx.RespondAsync($"🏓 Ping: {ctx.Client.Ping}ms");
+        }
+    }
+}
