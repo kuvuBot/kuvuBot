89e855bb53b1f03444dd88389e4eab50299aceff
js6pak <kubastaron@hotmail.com>
2018-12-28 00:02:51 +0100

    Add rps command

---
+++ kuvuBot/Commands/Fun/RPSCommand.cs
@@ -0,0 +1,80 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using HSNXT.DSharpPlus.ModernEmbedBuilder;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kuvuBot.Commands.Fun
+{
+    public class RPSCommand : BaseCommandModule
+    {
+        [Aliases("kpn")]
+        [Command("rps"), Description("Play rock, paper and scissors with bot")]
+        public async Task RPS(CommandContext ctx, string thing)
+        {
+            var things = new Dictionary<string, string>()
+            {
+                { "rock", "⚪ Rock" },
+                { "paper", "📰 Paper" },
+                { "scissors", "✂ Scissors" },
+            };
+
+            var results = new Dictionary<string, string>()
+            {
+                { "win", "🎉 You won!" },
+                { "lost", "🥊 You lost!" },
+                { "remis", "🏳 Remis!" },
+            };
+
+            thing = thing.Replace("rock", things["rock"], StringComparison.CurrentCultureIgnoreCase);
+            thing = thing.Replace("paper", things["paper"], StringComparison.CurrentCultureIgnoreCase);
+            thing = thing.Replace("scissors", things["scissors"], StringComparison.CurrentCultureIgnoreCase);
+
+            if (things.Values.Contains(thing))
+            {
+                string result = null;
+                var botthing = things.Values.ToArray()[new Random().Next(0, things.Count)];
+                
+                if(thing == botthing)
+                {
+                    result = results["remis"];
+                }else
+                // user beat bot
+                if ((thing == things["rock"] && botthing == things["scissors"]) 
+                    || (thing == things["paper"] && botthing == things["rock"])
+                    || (thing == things["scissors"] && botthing == things["paper"]))
+                {
+                    result = results["win"];
+                }else
+                // bot beat user
+                if ((botthing == things["rock"] && thing == things["scissors"])
+                    || (botthing == things["paper"] && thing == things["rock"])
+                    || (botthing == things["scissors"] && thing == things["paper"]))
+                {
+                    result = results["lost"];
+                }
+
+                var message = await new ModernEmbedBuilder
+                {
+                    Title = "Rock, paper and scissors",
+                    Color = new DuckColor(33, 150, 243),
+                    Timestamp = DuckTimestamp.Now,
+                    Fields =
+                    {
+                        ("You", thing, true),
+                        ("Bot", botthing, true),
+                        ("Result", result, true)
+                    },
+                    Footer = ($"Generated for {ctx.User.Username}#{ctx.User.Discriminator}", ctx.User.AvatarUrl),
+                }.Send(ctx.Message.Channel);
+            }
+            else
+            {
+                await ctx.RespondAsync("Stop cheater! You can use only `rock`/`paper`/`scissors`");
+            }
+        }
+    }
+}
--- kuvuBot/Commands/LogController.cs
+++ kuvuBot/Commands/LogController.cs
@@ -62,46 +62,46 @@ namespace kuvuBot.Commands
             if (user != null)
                 embed.Author = ($"{user.Name()} | {user.Id}", null, user.AvatarUrl ?? user.DefaultAvatarUrl);
             await embed.Send(discordChannel);
-    }
+        }
 
-    private async static Task Client_GuildMemberRemoved(GuildMemberRemoveEventArgs e)
-    {
-        var kuvuGuild = await e.Guild.GetKuvuGuild();
-        if (kuvuGuild.LogChannel.HasValue)
+        private async static Task Client_GuildMemberRemoved(GuildMemberRemoveEventArgs e)
         {
-            var channel = e.Guild.GetChannel(kuvuGuild.LogChannel.Value);
-            await Log("⬅", $"{e.Member.Name()} leave guild", kuvuGuild, channel, e.Member);
+            var kuvuGuild = await e.Guild.GetKuvuGuild();
+            if (kuvuGuild.LogChannel.HasValue)
+            {
+                var channel = e.Guild.GetChannel(kuvuGuild.LogChannel.Value);
+                await Log("⬅", $"{e.Member.Name()} leave guild", kuvuGuild, channel, e.Member);
+            }
+            if (kuvuGuild.GoodbyeChannel.HasValue && !string.IsNullOrWhiteSpace(kuvuGuild.GoodbyeMessage))
+            {
+                var channel = e.Guild.GetChannel(kuvuGuild.GoodbyeChannel.Value);
+                await channel.SendMessageAsync(kuvuGuild.GoodbyeMessage
+                    .Replace("%name%", e.Member.DisplayName)
+                    .Replace("%discriminator%", e.Member.Discriminator)
+                    .Replace("%mention%", e.Member.Mention));
+            }
         }
-        if (kuvuGuild.GoodbyeChannel.HasValue && !string.IsNullOrWhiteSpace(kuvuGuild.GoodbyeMessage))
-        {
-            var channel = e.Guild.GetChannel(kuvuGuild.GoodbyeChannel.Value);
-            await channel.SendMessageAsync(kuvuGuild.GoodbyeMessage
-                .Replace("%name%", e.Member.DisplayName)
-                .Replace("%discriminator%", e.Member.Discriminator)
-                .Replace("%mention%", e.Member.Mention));
-        }
-    }
 
-    private async static Task Client_GuildMemberAdded(GuildMemberAddEventArgs e)
-    {
-        var kuvuGuild = await e.Guild.GetKuvuGuild();
-        if (kuvuGuild.LogChannel.HasValue)
+        private async static Task Client_GuildMemberAdded(GuildMemberAddEventArgs e)
         {
-            var channel = e.Guild.GetChannel(kuvuGuild.LogChannel.Value);
-            await Log("➡", $"{e.Member.Name()} joined guild", kuvuGuild, channel, e.Member);
-        }
-        if (kuvuGuild.GreetingChannel.HasValue && !string.IsNullOrWhiteSpace(kuvuGuild.GreetingMessage))
-        {
-            var channel = e.Guild.GetChannel(kuvuGuild.GoodbyeChannel.Value);
-            await channel.SendMessageAsync(kuvuGuild.GreetingMessage
-                .Replace("%name%", e.Member.DisplayName)
-                .Replace("%discriminator%", e.Member.Discriminator)
-                .Replace("%mention%", e.Member.Mention));
-        }
-        if (kuvuGuild.AutoRole.HasValue)
-        {
-            await e.Member.GrantRoleAsync(e.Guild.GetRole(kuvuGuild.AutoRole.Value));
+            var kuvuGuild = await e.Guild.GetKuvuGuild();
+            if (kuvuGuild.LogChannel.HasValue)
+            {
+                var channel = e.Guild.GetChannel(kuvuGuild.LogChannel.Value);
+                await Log("➡", $"{e.Member.Name()} joined guild", kuvuGuild, channel, e.Member);
+            }
+            if (kuvuGuild.GreetingChannel.HasValue && !string.IsNullOrWhiteSpace(kuvuGuild.GreetingMessage))
+            {
+                var channel = e.Guild.GetChannel(kuvuGuild.GoodbyeChannel.Value);
+                await channel.SendMessageAsync(kuvuGuild.GreetingMessage
+                    .Replace("%name%", e.Member.DisplayName)
+                    .Replace("%discriminator%", e.Member.Discriminator)
+                    .Replace("%mention%", e.Member.Mention));
+            }
+            if (kuvuGuild.AutoRole.HasValue)
+            {
+                await e.Member.GrantRoleAsync(e.Guild.GetRole(kuvuGuild.AutoRole.Value));
+            }
         }
     }
-}
 }
