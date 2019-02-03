883a773872ba1285f161f49891adbb074502eede
Jakub Staroń <kubastaron@hotmail.com>
2019-02-03 16:17:03 +0100

    Add invite command

---
+++ kuvuBot/Commands/Information/InviteCommand.cs
@@ -0,0 +1,23 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using HSNXT.DSharpPlus.ModernEmbedBuilder;
+
+namespace kuvuBot.Commands.Information
+{
+
+    public class InviteCommand : BaseCommandModule
+    {
+        [Command("invite"), Description("Send bot invite link")]
+        public async Task Invite(CommandContext ctx)
+        {
+            var app = await ctx.Client.GetCurrentApplicationAsync();
+            await ctx.RespondAsync(app.GenerateBotOAuth(DSharpPlus.Permissions.Administrator));
+        }
+    }
+
+}
