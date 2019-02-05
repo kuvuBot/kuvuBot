7e9ff56068be2ad3553631665cd6ab6acb2fb0fa
kuvuś <kuba17ism@tuta.io>
2019-02-05 21:35:22 +0100

    Remove unnecessary dependencies from ship command

--- kuvuBot/Commands/Fun/ShipCommand.cs
+++ kuvuBot/Commands/Fun/ShipCommand.cs
@@ -2,13 +2,9 @@
 using DSharpPlus.CommandsNext.Attributes;
 using DSharpPlus.Entities;
 using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using HSNXT.DSharpPlus.ModernEmbedBuilder;
-using kuvuBot.Lang;
 using SkiaSharp;
 
 namespace kuvuBot.Commands.Fun
