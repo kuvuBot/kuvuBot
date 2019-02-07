b7c4cd46ef389f2346d724d17510cd8fc0c8e934
kuvuś <kuba17ism@tuta.io>
2019-02-07 15:51:53 +0100

    Fix problemes with responsiveness

--- kuvuBot.Panel/wwwroot/js/site.js
+++ kuvuBot.Panel/wwwroot/js/site.js
@@ -2,3 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+$(".menu-toggle").click(function(e) {
+    e.preventDefault();
+    $(".main").toggleClass("toggled");
+});
+$(window).resize(() => {
+   let w = $(this);
+    let main = $(".main");
+   if (w.width() <= 980) {
+       console.log('aaa');
+       if (main.hasClass('toggled')) {
+           main.removeClass('toggled');
+           main.addClass('toggled-off');
+       }
+   } else if (w.width() > 980) {
+       if (main.hasClass('toggled-off')) {
+           main.removeClass('toggled-off');
+           main.addClass('toggled')
+       }
+   }
+});
