5a547bb5d6ecf91b01dc4b9024f207c745089d3a
Jakub Staroń <kubastaron@hotmail.com>
2019-02-08 15:58:47 +0100

    Standardize menu buttons

--- kuvuBot.Panel/wwwroot/js/site.js
+++ kuvuBot.Panel/wwwroot/js/site.js
@@ -3,23 +3,22 @@
 
 // Write your Javascript code.
 
-$(".menu-toggle").click(function(e) {
+$("#sidebar-toggle").click(function (e) {
     e.preventDefault();
     $(".main").toggleClass("toggled");
 });
 $(window).resize(() => {
-   let w = $(this);
+    let w = $(this);
     let main = $(".main");
-   if (w.width() <= 980) {
-       console.log('aaa');
-       if (main.hasClass('toggled')) {
-           main.removeClass('toggled');
-           main.addClass('toggled-off');
-       }
-   } else if (w.width() > 980) {
-       if (main.hasClass('toggled-off')) {
-           main.removeClass('toggled-off');
-           main.addClass('toggled')
-       }
-   }
+    if (w.width() <= 980) {
+        if (main.hasClass('toggled')) {
+            main.removeClass('toggled');
+            main.addClass('toggled-off');
+        }
+    } else if (w.width() > 980) {
+        if (main.hasClass('toggled-off')) {
+            main.removeClass('toggled-off');
+            main.addClass('toggled');
+        }
+    }
 });
