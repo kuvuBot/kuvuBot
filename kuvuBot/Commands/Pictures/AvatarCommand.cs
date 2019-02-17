0c405dcdc416bef35f088a8a2a50560a02c365da
Jakub Staroń <kubastaron@hotmail.com>
2019-02-17 13:32:37 +0100

    Add avatar command

---
+++ kuvuBot/Commands/Pictures/AvatarCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using HSNXT.DSharpPlus.ModernEmbedBuilder;
+using kuvuBot.Commands.Attributes;
+
+namespace kuvuBot.Commands.Pictures
+{
+    public class AvatarCommand : BaseCommandModule
+    {
+        [Command("avatar"), LocalizedDescription("avatar.description")]
+        [RequireBotPermissions(Permissions.SendMessages | Permissions.AttachFiles)]
+        public async Task Avatar(CommandContext ctx, [Description("User to get avatar")] DiscordUser target)
+        {
+            await ctx.Channel.TriggerTypingAsync();
+            await new ModernEmbedBuilder()
+            {
+                Title = $"{target.Name()}'s avatar",
+                ImageUrl = target.AvatarUrl ?? target.DefaultAvatarUrl,
+                Color = Program.Config.EmbedColor,
+            }.Send(ctx.Channel);
+        }
+    }
+}
