4884676e219c7ac96cbf68cfea1db9e4bbeef368
kuvuś <kuba17ism@tuta.io>
2020-01-03 20:22:26 +0100

    Create reverse command

---
+++ kuvuBot/Commands/Fun/ReverseCommand.cs
@@ -0,0 +1,21 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+using kuvuBot.Commands.Attributes;
+
+namespace kuvuBot.Commands.Fun
+{
+    public class ReverseCommand : BaseCommandModule
+    {
+        [RequireBotPermissions(Permissions.SendMessages)]
+        [Command("reverse"), LocalizedDescription("reverse.description"), Aliases("odwróć")]
+        public async Task ReverseText(CommandContext ctx, [RemainingText] string message)
+        {
+            var charArray = message.ToCharArray();
+            Array.Reverse( charArray );
+            await ctx.RespondAsync(charArray.ToString());
+        }
+    }
+}
