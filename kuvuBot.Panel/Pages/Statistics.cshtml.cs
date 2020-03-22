4296ce09fa63687ac2812bb061c9483ed29599f1
Jakub Staroń <kubastaron@hotmail.com>
2020-03-22 20:54:07 +0100

    Start working on new website

--- kuvuBot.Panel/Pages/Statistics.cshtml.cs
+++ kuvuBot.Panel/Pages/Statistics.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,9 +10,25 @@ namespace kuvuBot.Panel.Pages
 {
     public class StatisticsModel : PageModel
     {
-        public void OnGet()
+        private async Task<double> GetCpuUsage()
         {
+            var startTime = DateTime.UtcNow;
+            var startCpuUsage = Process.GetCurrentProcess().TotalProcessorTime;
+            await Task.Delay(500);
 
+            var endTime = DateTime.UtcNow;
+            var endCpuUsage = Process.GetCurrentProcess().TotalProcessorTime;
+            var cpuUsedMs = (endCpuUsage - startCpuUsage).TotalMilliseconds;
+            var totalMsPassed = (endTime - startTime).TotalMilliseconds;
+            var cpuUsageTotal = cpuUsedMs / (Environment.ProcessorCount * totalMsPassed);
+            return cpuUsageTotal * 100;
+        }
+
+        public string CpuUsage { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            this.CpuUsage = (await GetCpuUsage()).ToString("F1");
         }
     }
 }
