214914a88af9f30848746043eb5693cc901ef80d
Jakub Staroń <kubastaron@hotmail.com>
2020-04-08 18:08:37 +0200

    Rewrite leaderboard

--- kuvuBot/Commands/CommandUtils.cs
+++ kuvuBot/Commands/CommandUtils.cs
@@ -132,5 +132,10 @@ namespace kuvuBot.Commands
             }
             return null;
         }
+
+        public static async Task<IEnumerable<TResult>> SelectAsync<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, Task<TResult>> method)
+        {
+            return await Task.WhenAll(source.Select(async s => await method(s)));
+        }
     }
 }
