ad443be0b8bac1c73568657ac63e47e214965037
Jakub Staroń <kubastaron@hotmail.com>
2020-05-14 13:17:21 +0200

    Fix figlet

--- kuvuBot/Commands/Fun/FigletCommand.cs
+++ kuvuBot/Commands/Fun/FigletCommand.cs
@@ -1,30 +1,45 @@
-﻿using DSharpPlus.CommandsNext;
+﻿using System;
+using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
 using System.Threading.Tasks;
 using Colorful;
 using System.Reflection;
-using System.IO;
 using DSharpPlus;
+using HSNXT.DSharpPlus.ModernEmbedBuilder;
 using kuvuBot.Commands.Attributes;
 using kuvuBot.Core.Commands;
+using kuvuBot.Lang;
 
 namespace kuvuBot.Commands.Fun
 {
     public class FigletCommand : BaseCommandModule
     {
+        private FigletFont FigletFont { get; }
+
+        public FigletCommand()
+        {
+            FigletFont = FigletFont.Load(Assembly.GetExecutingAssembly().GetManifestResourceStream("kuvuBot.big.flf"));
+        }
+
         [Command("figlet"), LocalizedDescription("figlet.description")]
         [RequireBotPermissions(Permissions.SendMessages)]
         public async Task Figlet(CommandContext ctx, [RemainingText] string message)
         {
             message.RequireRemainingText();
-            // load figlet font from assembly (.exe)
-            var assembly = Assembly.GetExecutingAssembly();
-            var resourceName = "kuvuBot.big.flf";
 
-            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            var figlet = new Figlet(FigletFont);
+
+            try
             {
-                var figlet = new Figlet(FigletFont.Load(stream));
                 await ctx.RespondAsync($"```{figlet.ToAscii(message).ConcreteValue}```");
+            }
+            catch (ArgumentException)
+            {
+                await new ModernEmbedBuilder
+                {
+                    Color = EmbedUtils.Red,
+                    Description = await ctx.Lang("figlet.specialCharacters")
+                }.AddGeneratedForFooter(ctx, false).Send(ctx.Channel);
             }
         }
     }
